1a8f02f89484d7dec71603dcbedb36bc554b225e
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-02-10 14:40:04 +0600

    fixed gitignore

--- backend/app/messages.js
+++ backend/app/messages.js
@@ -4,17 +4,17 @@ const fileDb = require('../fileDb');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const items = await fileDb.getMessages();
-    res.send(items);
+    const messages = await fileDb.getMessages();
+    res.send(messages);
 });
 
 router.post('/', async (req, res) => {
-    if (req.body.author === '' || req.body.message === '') {
-        res.status(400).send({"error": "Author and message must be present in the request"})
-    } else {
+    // if (req.body.author === '' || req.body.message === '') {
+    //     res.status(400).send({"error": "Author and message must be present in the request"})
+    // } else {
         await fileDb.addMessage(req.body);
-        res.send(req.body.message);
-    }
+        res.send(req.body.id);
+
 });
 
 
