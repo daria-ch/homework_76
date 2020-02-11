0a87f8a193b377a10f05ae02c3f517c829f308a5
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-02-11 14:09:55 +0600

    added ability to catch error

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,24 +1,36 @@
 import axiosApi from "../axios-api";
 
 export const FETCH_MESSAGES_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
-export const ADD_MESSAGE_SUCCESS = 'ADD_MESSAGE_SUCCESS';
+export const FETCH_MESSAGES_FAILURE = 'FETCH_PRODUCTS_FAILURE';
+export const SEND_MESSAGE_SUCCESS = 'SEND_MESSAGE_SUCCESS';
+export const SEND_MESSAGE_FAILURE = 'SEND_MESSAGE_FAILURE';
 
 
 export const fetchMessagesSuccess = messages => ({type: FETCH_MESSAGES_SUCCESS, messages});
-export const sendMessageSuccess = () => ({type: ADD_MESSAGE_SUCCESS});
+export const fetchMessagesFailure = error => ({type: FETCH_MESSAGES_FAILURE, error});
+export const sendMessageSuccess = () => ({type: SEND_MESSAGE_SUCCESS});
+export const sendMessageFailure = error => ({type: SEND_MESSAGE_FAILURE, error});
 
 
 export const fetchMessages = () => {
     return async (dispatch) => {
-        const response = await axiosApi.get('/messages');
-        dispatch(fetchMessagesSuccess(response.data));
+        try {
+            const response = await axiosApi.get('/messages');
+            dispatch(fetchMessagesSuccess(response.data));
+        } catch (error) {
+            dispatch(fetchMessagesFailure(error.response.data.error))
+        }
     }
 };
 
 
 export const sendMessage = (message) => {
-    return async (dispatch) => {
-        await axiosApi.post('/messages', message);
-        dispatch(sendMessageSuccess());
-    };
+    return async dispatch => {
+        try {
+            await axiosApi.post('/messages', message);
+            dispatch(sendMessageSuccess());
+        } catch (error) {
+            dispatch(sendMessageFailure(error.response.data.error))
+        }
+    }
 };
--- frontend/src/store/reducer.js
+++ frontend/src/store/reducer.js
@@ -1,13 +1,19 @@
-import {FETCH_MESSAGES_SUCCESS} from "./actions";
+import {FETCH_MESSAGES_FAILURE, FETCH_MESSAGES_SUCCESS, SEND_MESSAGE_FAILURE} from "./actions";
 
 const initialState = {
-    messages: []
+    messages: [],
+    lastDateTime: '',
+    error: null
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_MESSAGES_SUCCESS:
             return {...state, messages: action.messages};
+        case  FETCH_MESSAGES_FAILURE:
+            return {...state, error: action.error};
+        case SEND_MESSAGE_FAILURE:
+            return {...state, error: action.error};
         default:
             return state;
     }
